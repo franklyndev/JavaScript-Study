2ffdda7118531fcff1211ba0821c4ba42b16750b
FranklynSantos <franklynsantos451@gmail.com>
2025-10-30 15:21:18 -0300

    Antes e depois
    Ver o número antecessor e sucessor

---
+++ desafios/d003/script.js
@@ -0,0 +1,4 @@
+function digitar() {
+    let numero = Number(prompt('Digite um número inteiro qualquer'))
+    window.alert(`Antes de ${numero}, temos o número ${numero-1}.\nDepois de ${numero}, temos o número ${numero+1}`)
+}

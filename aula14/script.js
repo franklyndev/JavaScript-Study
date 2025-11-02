628c3758979d097696347631d625bc77f8855e2a
FranklynSantos <franklynsantos451@gmail.com>
2025-11-01 22:28:18 -0300

    pequenas mudanças

--- aula14/script.js
+++ aula14/script.js
@@ -2,15 +2,17 @@ function verificar() {
     let data = new Date()
     let ano = data.getFullYear()
     let txtAno = window.document.getElementById('txtAno')
-    let masc = window.document.getElementById('masc')
-    let fem = window.document.getElementById('fem')
-    let foto = window.document.getElementById('foto')
+    let descricao = window.document.getElementById('descricao')
+    
 
-
-    if (txtAno.value.length == 0 || txtAno.value > ano) {
+    if (txtAno.value.length == 0 || txtAno > ano) {
         alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         let sex = document.getElementsByName('radsex')
-        let idade = ano - txtAno.value
+        let idade = ano - Number(txtAno.value)
+    
+            descricao.innerHTML = `Idade: ${idade} `
+        
     }
+
 }
